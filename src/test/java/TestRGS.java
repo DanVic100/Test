2a428eea844a4f15631f068efda10b2acd4f3216
DanVic100 <greenpeace86@ya.ru>
2018-05-08 01:40:16 +0300

    PageObject

--- src/test/java/TestRGS.java
+++ src/test/java/TestRGS.java
@@ -1,108 +1,180 @@
-import org.junit.After;
+
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class TestRGS {
+public class TestRGS extends BasePage{
 
-    WebDriver webDriver;
 
-    @Before
-    public void setUp()
+
+
+
+    @FindBy(xpath = "//a[@data-toggle='dropdown' and contains(text(),'Страхование')]")
+    public WebElement stahovanie;
+
+    @FindBy(xpath = "//a[contains(text(),'ДМС')]")
+    private WebElement DMS;
+
+    @FindBy(xpath =  "//span[contains(text(),'Добровольное')]")
+    private WebElement title;
+
+    @FindBy(xpath =  "//a[contains(text(),'Отправить ')]")
+    private WebElement proposalButton;
+
+    @FindBy(xpath =  "//b")
+    private WebElement proposal;
+
+    @FindBy(xpath = "//input[@name='LastName']")
+    private WebElement lastName;
+
+    @FindBy(xpath = "//input[@name='FirstName']")
+    private WebElement firstName;
+
+    @FindBy(xpath = "//input[@name='MiddleName']")
+    private WebElement middleName;
+
+
+    @FindBy(xpath = "//label[contains(text(),'Телефон')]/../input")
+    private WebElement tel;
+
+
+    @FindBy(xpath = "//label[contains(text(),'почта')]/../input")
+    private WebElement email;
+
+    @FindBy(xpath = "//textarea")
+    private WebElement textArea;
+
+    @FindBy(xpath = "//select")
+    private WebElement region;
+
+    @FindBy(xpath = "//input[@name='ContactDate']")
+    private WebElement dateContackt;
+
+    @FindBy(xpath = "//*[@class='checkbox']")
+    private WebElement checkbox;
+
+    @FindBy(xpath = "//*[@id=\"button-m\"]")
+    private WebElement buttomEmail;
+
+    @FindBy(xpath = "//span[contains(text(),'адрес')]")
+    private WebElement textError;
+
+
+
+    private TestRGS clickStrahovanie()
     {
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
-        webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
-        webDriver.get("http://rgs.ru");
+        stahovanie.click();
+        return this;
     }
 
-    @After
-    public void tearDown()
+    private TestRGS clickDMS()
     {
-        webDriver.quit();
+        //щелкаем на ДМС
+        DMS.click();
+        webDriver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+        return this;
     }
+
+    private TestRGS assertDMS()
+    {
+        //проверяем титульник
+        Assert.assertEquals("Титульник страницы соответстувет выбору в селекте",
+                "Добровольное медицинское страхование (ДМС)", title.getText());
+        return this;
+    }
+
+    private TestRGS proposalButtonClick()
+    {
+        //проверяем титульник
+        proposalButton.click();
+        webDriver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+        return this;
+    }
+
+    private TestRGS assertTitle()
+    {
+        //проверка титульника
+        wait.until(ExpectedConditions.visibilityOf(proposal));
+        Assert.assertEquals("Титульник страницы не соответстувет выбору в селекте",
+                "Заявка на добровольное медицинское страхование", proposal.getText());
+        return this;
+    }
+
+    private TestRGS enterField()
+    {
+        lastName.sendKeys("Иванов");
+        middleName.sendKeys("Иванович");
+        firstName.sendKeys("Ivan");
+        email.sendKeys("qwertyqwerty");
+        tel.sendKeys("1234567890");
+
+        dateContackt.sendKeys("21112018");
+
+        textArea.sendKeys("Comment");
+
+        return this;
+    }
+
+    private TestRGS selectRegion()
+    {
+        Select select = new Select(region);
+        select.selectByValue("77");
+
+        return this;
+    }
+
+    private TestRGS checkBoxClick()
+    {
+        checkbox.click();
+        return  this;
+    }
+
+    private TestRGS assertField()
+    {
+        Assert.assertEquals("Иванов", lastName.getAttribute("value"));
+        Assert.assertEquals("Ivan", firstName.getAttribute("value"));
+        Assert.assertEquals("Иванович", middleName.getAttribute("value"));
+        Assert.assertEquals("+7 (123) 456-78-90",tel.getAttribute("value"));
+        Assert.assertEquals("qwertyqwerty", email.getAttribute("value"));
+        Assert.assertEquals("21.11.2018", dateContackt.getAttribute("value"));
+        Assert.assertEquals("Comment", textArea.getAttribute("value"));
+        Assert.assertEquals("77", region.getAttribute("value"));
+
+        return  this;
+    }
+
+    private TestRGS checkButtomEmail()
+    {
+        buttomEmail.click();
+        return  this;
+    }
+
+    private TestRGS assertErrorText()
+    {
+       Assert.assertEquals("Титульник страницы не соответстувет выбору в селекте",
+                "Введите адрес электронной почты", textError.getText());
+        return  this;
+    }
+
 
     @Test
     public void testRGS() throws InterruptedException
     {
-        //ищем элемент "страхование" и кликаем
-        WebElement webElement = webDriver.findElement(By.xpath("//*[@id=\"main-navbar-collapse\"]/ol[1]/li[2]"));
-       // new Actions(webDriver).moveToElement(webElement).perform();
-        webElement.click();
-
-        //ждем пока загрузится
-        WebDriverWait wait = new WebDriverWait(webDriver, 3);
-        wait.until(ExpectedConditions.visibilityOf(webDriver.findElement(By.xpath("//*[@id=\"rgs-main-menu-insurance-dropdown\"]/div[1]/div[1]/div/div[1]/div[3]/ul/li[2]/a"))));
-
-        //щелкаем на ДМС
-        WebElement DMS = webDriver.findElement(By.xpath("//*[@id=\"rgs-main-menu-insurance-dropdown\"]/div[1]/div[1]/div/div[1]/div[3]/ul/li[2]/a"));
-        DMS.click();
-        webDriver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
-
-        //проверяем титульник
-        WebElement title = webDriver.findElement(By.xpath("//*[@id=\"main-content\"]/div[2]/div/span"));
-        Assert.assertEquals("Титульник страницы соответстувет выбору в селекте",
-                "Добровольное медицинское страхование (ДМС)", title.getText());
-
-        //*[@id="rgs-main-context-bar"]/div/div/div/a[3]
-        WebElement  proposalButton= webDriver.findElement(By.xpath("//*[@id=\"rgs-main-context-bar\"]/div/div/div/a[3]"));
-        proposalButton.click();
-        wait.until(ExpectedConditions.visibilityOf(webDriver.findElement(By.xpath("//html/body/div[7]/div/div/div/div[1]/h4/b"))));
-
-        //проверка титульника
-        WebElement proposal = webDriver.findElement(By.xpath("//html/body/div[7]/div/div/div/div[1]/h4/b"));
-        Assert.assertEquals("Титульник страницы не соответстувет выбору в селекте",
-                "Заявка на добровольное медицинское страхование", proposal.getText());
-
-
-        List<WebElement> email = webDriver.findElements(By.xpath( "//*[@class=\"form-control\"]"));
-        email.get(0).sendKeys("Иванов");
-        email.get(1).sendKeys("Иван");
-        email.get(2).sendKeys("Иванович");
-        email.get(3).sendKeys("1234567890");
-        email.get(4).sendKeys("qwertyqwerty");
-
-        WebElement textArea = webDriver.findElement(By.xpath("//textarea"));
-        textArea.sendKeys("Comment");
-
-        WebElement region = webDriver.findElement(By.xpath("//*[@id=\"applicationForm\"]/div[2]/div[4]/select"));
-        Select select = new Select(region);
-        select.selectByValue("77");
-
-        //date contact
-        WebElement dateContackt = webDriver.findElement(By.xpath("//*[@id=\"applicationForm\"]/div[2]/div[7]/input"));
-        dateContackt.sendKeys("21112018");
-
-        WebElement checkbox = webDriver.findElement(By.xpath("//*[@class='checkbox']"));
-        checkbox.click();
-
-        Assert.assertEquals("Иванов", webDriver.findElement(By.name("LastName")).getAttribute("value"));
-        Assert.assertEquals("Иван", webDriver.findElement(By.name("FirstName")).getAttribute("value"));
-        Assert.assertEquals("Иванович", webDriver.findElement(By.name("MiddleName")).getAttribute("value"));
-        Assert.assertEquals("+7 (123) 456-78-90",webDriver.findElement(By.xpath("//*[@id=\"applicationForm\"]/div[2]/div[5]/input")).getAttribute("value"));
-        Assert.assertEquals("qwertyqwerty", webDriver.findElement(By.name("Email")).getAttribute("value"));
-        Assert.assertEquals("21.11.2018", webDriver.findElement(By.xpath("//*[@id=\"applicationForm\"]/div[2]/div[7]/input")).getAttribute("value"));
-        Assert.assertEquals("Comment", webDriver.findElement(By.xpath("//textarea")).getAttribute("value"));
-        Assert.assertEquals("77", webDriver.findElement(By.xpath("//*[@id=\"applicationForm\"]/div[2]/div[4]/select")).getAttribute("value"));
-
-        //жмем кнопку отправить
-        WebElement buttomEmail = webDriver.findElement(By.xpath("//*[@id=\"button-m\"]"));
-        buttomEmail.click();
-
-        WebElement textError = webDriver.findElement(By.xpath("//*[@id=\"applicationForm\"]/div[2]/div[6]/div/label/span"));
-        Assert.assertEquals("Титульник страницы не соответстувет выбору в селекте",
-                "Введите адрес электронной почты", textError.getText());
-
-
+        this.clickStrahovanie()
+            .clickDMS()
+            .assertDMS()
+            .proposalButtonClick()
+            .assertTitle()
+            .enterField()
+            .selectRegion()
+            .checkBoxClick()
+            .assertField()
+            .checkButtomEmail()
+            .assertErrorText();
     }
 }
